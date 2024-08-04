9a26f5ef02134d2c53e24b4f063bd4775a340044
scrogagtl <scott@scottandrewrogers.com>
2024-08-04 22:06:35 +0000

    adding alternate world servce in ruby

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -4,23 +4,39 @@ from flask import request
 import os
 from datetime import datetime
 from time import sleep
-import os
+from random import randrange
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
 helloHost = os.environ.get('HELLO_HOST', "localhost")
 worldHost = os.environ.get('WORLD_HOST', "localhost")
-endpoint  = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', "localhost")
+worldHostRuby = os.environ.get('WORLD_HOST_RUBY', "localhost")
+worldPort = '5002'
+# endpoint  = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', "localhost")
 shard = os.environ.get('SHARD', "na")
 print(" [frontend: " + shard + "] - " + " initialized]")
 
-
+def which_world():
+    global worldHost
+    global worldHostRuby
+    global worldPort
+    r = randrange(100)
+    timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    print(timeString + " - [frontend: " + shard + "] - " + " random=" + str(r))
+    if r > 49:
+        return worldHostRuby, '5003'
+    else: 
+        return worldHost, '5002'
 
 @app.route("/")
 def front_end():
     timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-    resLocal = timeString 
-    res = " - [frontend: " + shard + " - hello host: " + helloHost + " ]"
+    # res = " - [frontend: " + shard + " ]- hello host: " + helloHost + " ]"
+    res = " - [frontend: " + shard + "]"
     try:
         resH = requests.get('http://' + helloHost + ':5001')
         if resH.status_code >= 300:
@@ -31,7 +47,9 @@ def front_end():
         res += " hello status: " + repr(e)
 
     try: 
-        resW = requests.get('http://' + worldHost + ':5002')
+        lHost, lPort = which_world()
+        resW = requests.get('http://' + lHost + ':' + lPort)
+        
         if resW.status_code >= 300:
             res += " | world status: " + str(resW.status_code) + " - " + resW.text + " - " + " | worldHost: " + worldHost
         else:
@@ -39,6 +57,5 @@ def front_end():
     except Exception as e:
         res += " world status:" + repr(e)
 
-
-    print (resLocal + res)
+    print (timeString + res)
     return res
